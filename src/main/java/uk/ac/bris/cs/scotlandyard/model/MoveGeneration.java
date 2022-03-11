e66a6b782d2c7a7481138926ca87b54e3e5b9ece
Samson Main <mains03prog@gmail.com>
2022-03-11 15:28:23 +0000

    Finished SingleMoveGeneration implementation

--- src/main/java/uk/ac/bris/cs/scotlandyard/model/MoveGeneration.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/model/MoveGeneration.java
@@ -1,8 +1,12 @@
 package uk.ac.bris.cs.scotlandyard.model;
 
 import com.google.common.collect.ImmutableSet;
+import com.google.common.graph.ImmutableValueGraph;
 
+import javax.annotation.concurrent.Immutable;
 import java.util.Objects;
+import java.util.Optional;
+import java.util.concurrent.TransferQueue;
 
 public interface MoveGeneration {
     public ImmutableSet<Move> generateMoves();
@@ -10,27 +14,123 @@ public interface MoveGeneration {
     public final class SingleMoveGeneration implements MoveGeneration {
         private final ImmutableSet<Move.SingleMove> moves;
 
-        public SingleMoveGeneration(final Player player) {
+        public SingleMoveGeneration(
+                final ImmutableValueGraph<Integer, ImmutableSet<ScotlandYard.Transport>> graph,
+                final Player player
+        ) {
+            Objects.requireNonNull(graph);
             Objects.requireNonNull(player);
-            moves = generateMoves(player);
+            moves = generateMoves(graph, player);
         }
 
-        private ImmutableSet<Move.SingleMove> generateMoves(final Player player) {
+        private ImmutableSet<Move.SingleMove> generateMoves(
+                final ImmutableValueGraph<Integer, ImmutableSet<ScotlandYard.Transport>> graph,
+                final Player player
+        ) {
             if (player.isMrX()) {
-                return generateMrXMoves(player);
+                return generateMrXMoves(graph, player);
             } else {
-                return generateDetectiveMoves(player);
+                return generateDetectiveMoves(graph, player);
             }
         }
 
-        private ImmutableSet<Move.SingleMove> generateMrXMoves(final Player player) {
-            if (!player.isMrX()) { throw new IllegalArgumentException(); }
-            return null;
+        private ImmutableSet<Move.SingleMove> generateMrXMoves(
+                final ImmutableValueGraph<Integer, ImmutableSet<ScotlandYard.Transport>> graph,
+                final Player player
+        ) {
+            ImmutableSet.Builder<Move.SingleMove> builder = new ImmutableSet.Builder<>();
+            graph.adjacentNodes(player.location()).stream().forEach(destination -> {
+                builder.addAll(generateMrXMovesToDestination(
+                        graph,
+                        player,
+                        destination
+                ));
+            });
+
+            return builder.build();
         }
 
-        private ImmutableSet<Move.SingleMove> generateDetectiveMoves(final Player player) {
-            if (!player.isDetective()) { throw new IllegalArgumentException(); }
-            return null;
+        private ImmutableSet<Move.SingleMove> generateGeneralMovesToDestination(
+                final ImmutableValueGraph<Integer, ImmutableSet<ScotlandYard.Transport>> graph,
+                final Player player,
+                Integer destination
+        ) {
+            ImmutableSet.Builder<Move.SingleMove> builder = new ImmutableSet.Builder<>();
+            graph.edgeValue(player.location(), destination).ifPresent(allTransport -> allTransport.stream()
+                    .forEach(transport -> {
+                        generateGeneralMoveFromTransport(player, destination, transport).ifPresent(move -> {
+                            builder.add(move);
+                        });
+                    })
+            );
+            return builder.build();
+        }
+
+        private Optional<Move.SingleMove> generateGeneralMoveFromTransport(
+                final Player player,
+                Integer destination,
+                ScotlandYard.Transport transport
+        ) {
+            if (player.has(transport.requiredTicket())) {
+                return Optional.of(new Move.SingleMove(
+                        player.piece(),
+                        player.location(),
+                        transport.requiredTicket(),
+                        destination
+                ));
+            } else {
+                return Optional.empty();
+            }
+        }
+
+        private ImmutableSet<Move.SingleMove> generateMrXMovesToDestination(
+                final ImmutableValueGraph<Integer, ImmutableSet<ScotlandYard.Transport>> graph,
+                final Player player,
+                Integer destination
+        ) {
+            ImmutableSet.Builder<Move.SingleMove> builder = new ImmutableSet.Builder<>();
+            builder.addAll(generateGeneralMovesToDestination(graph, player, destination));
+            generateMrXSecretMove(player, destination).ifPresent(move -> {
+                builder.add(move);
+            });
+            return builder.build();
+        }
+
+        private Optional<Move.SingleMove> generateMrXSecretMove(
+                final Player player,
+                Integer destination
+        ) {
+            if (player.has(ScotlandYard.Ticket.SECRET)) {
+                return Optional.of(new Move.SingleMove(
+                        player.piece(),
+                        player.location(),
+                        ScotlandYard.Ticket.SECRET,
+                        destination
+                ));
+            } else {
+                return Optional.empty();
+            }
+        }
+
+        private ImmutableSet<Move.SingleMove> generateDetectiveMoves(
+                final ImmutableValueGraph<Integer, ImmutableSet<ScotlandYard.Transport>> graph,
+                final Player player
+        ) {
+            ImmutableSet.Builder<Move.SingleMove> builder = new ImmutableSet.Builder<>();
+            graph.adjacentNodes(player.location()).stream().forEach(destination -> {
+                builder.addAll(generateDetectiveMovesToDestination(graph, player, destination));
+            });
+            return builder.build();
+        }
+
+        private ImmutableSet<Move.SingleMove> generateDetectiveMovesToDestination(
+                final ImmutableValueGraph<Integer, ImmutableSet<ScotlandYard.Transport>> graph,
+                final Player player,
+                Integer destination
+        ) {
+            ImmutableSet.Builder<Move.SingleMove> builder = new ImmutableSet.Builder<>();
+            builder.addAll(generateGeneralMovesToDestination(graph, player, destination));
+            return builder.build();
         }
 
         public ImmutableSet<Move.SingleMove> getSingleMoves() {
@@ -46,10 +146,14 @@ public interface MoveGeneration {
     public class DoubleMoveGeneration implements MoveGeneration {
         private final ImmutableSet<Move.DoubleMove> moves;
 
-        public DoubleMoveGeneration(final Player player) {
+        public DoubleMoveGeneration(
+                final ImmutableValueGraph<Integer, ImmutableSet<ScotlandYard.Transport>> graph,
+                final Player player
+        ) {
+            Objects.requireNonNull(graph);
             Objects.requireNonNull(player);
             if (!player.isMrX()) { throw new IllegalArgumentException(); }
-            moves = generateMoves(player, new SingleMoveGeneration(player).getSingleMoves());
+            moves = generateMoves(player, new SingleMoveGeneration(graph, player).getSingleMoves());
         }
 
         private ImmutableSet<Move.DoubleMove> generateMoves(final Player player, ImmutableSet<Move.SingleMove> singleMoves) {
